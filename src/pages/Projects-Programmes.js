22f62120501503ec6b5f4d90bb96522c93b9d2e9
Ken Joo <kenjoo.yeap@gmail.com>
2021-05-13 13:24:36 +0100

    styling: Project programmers page

--- src/pages/Projects-Programmes.js
+++ src/pages/Projects-Programmes.js
@@ -19,20 +19,20 @@ const ProjectProgrammes = () => {
                 text = "With projects and programmes located across the Borderlands, our strategic approach means our projects reinforce each other to transform our area. Some are in distinct locations making the most of an existing asset or creating something new whilst others will be delivered across the full area as part of the overarching Deal themes."
             />
             <div className="container">
-                <div>The projects and programmes included in the Deal&nbsp;are shown in this map of our region</div>
+                <div className="title sub">The projects and programmes included in the Deal&nbsp;are shown in this map of our region</div>
                 <img src={Map} alt="Banner" className="banner"/>
             <div><strong>&pound;30m of advanced funding has already been released, for projects which have achieved full business case sign off. These projects are:</strong></div>
 
             <ul>
-                <li>Ad Gefrin Visitor Experience and Distillery in Wooler (<a href="https://adgefrin.co.uk/">find out more</a>)</li>
-                <li>Lillidorei at The Alnwick Garden (<a href="https://www.alnwickgarden.com/">find out more</a>)</li>
+                <li>Ad Gefrin Visitor Experience and Distillery in Wooler (<a className="anchor_style" href="https://adgefrin.co.uk/">find out more</a>)</li>
+                <li>Lillidorei at The Alnwick Garden (<a className="anchor_style" href="https://www.alnwickgarden.com/">find out more</a>)</li>
                 <li>Phase 1 of the Carlisle Station Gateway and Citadels</li>
                 <li>Development of the Borderlands Energy Masterplan</li>
-                <li>Digital voucher top up scheme in Cumbria and Northumberland (<a href="/Digital-Voucher-Scheme">find out more</a>)</li>
+                <li>Digital voucher top up scheme in Cumbria and Northumberland (<a className="anchor_style" href="/Digital-Voucher-Scheme">find out more</a>)</li>
             </ul>
             </div>
 
-            <div>More information on each of the divrojects and programmes listed below is available within the&nbsp;<a href="/Portals/0/Borderlands%20Inclusive%20Growth%20Deal%20Deal%20Document%202021%20-%20Signed.pdf?ver=2021-03-17-183958-067">Deal Document</a>.</div>
+            <p>More information on each of the divrojects and programmes listed below is available within the&nbsp;<a className="anchor_style" href="/Portals/0/Borderlands%20Inclusive%20Growth%20Deal%20Deal%20Document%202021%20-%20Signed.pdf?ver=2021-03-17-183958-067">Deal Document</a>.</p>
             <img src={Bottom} alt="Banner" className="banner"/>
             <Footer/>
         </>
