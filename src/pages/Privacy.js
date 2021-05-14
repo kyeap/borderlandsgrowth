61e5bc53802f4e748e5a544a6936db8b753b07c7
Ken Joo <kenjoo.yeap@gmail.com>
2021-05-14 16:01:23 +0100

    styling: privacy and terms

--- src/pages/Privacy.js
+++ src/pages/Privacy.js
@@ -7,98 +7,101 @@ const Privacy = () => {
     return (
         <>
             <Header/>
-            <p>Borderlands is committed to protecting your privacy and developing technology
-            that gives you the most powerful and safe online experience. This Statement of Privacy
-            applies to the Borderlands site and governs data collection and usage.
-            By using the Borderlands site, you consent to the data practices described
-            in this statement.</p>
-            <p><span>Collection of your Personal Information</span></p>
-            <p>Borderlands collects personally identifiable information, such as your email
-            address, name, home or work address or telephone number. Borderlands also
-            collects anonymous demographic information, which is not unique to you, such as
-            your ZIP code, age, gender, preferences, interests and favorites.</p>
-            <p>There is also information about your computer hardware and software that is automatically
-            collected by Borderlands. This information can include: your IP address,
-            browser type, domain names, access times and referring website addresses. This
-            information is used by Borderlands for the operation of the service, to
-            maintain quality of the service, and to provide general statistics regarding use
-            of the Borderlands site.</p>
-            <p>Please keep in mind that if you directly disclose personally identifiable information
-            or personally sensitive data through Borderlands public message boards,
-            this information may be collected and used by others. Note: Borderlands
-            does not read any of your private online communications.</p>
-            <p>Borderlands encourages you to review the privacy statements of Web sites
-            you choose to link to from Borderlands so that you can understand how those
-            Web sites collect, use and share your information. Borderlands is not responsible
-            for the privacy statements or other content on Web sites outside of the Borderlands
-            and Borderlands family of Web sites.</p>
-            <p><span>Use of your Personal Information</span></p>
-            <p>Borderlands collects and uses your personal information to operate the Borderlands
-            Web site and deliver the services you have requested. Borderlands also uses
-            your personally identifiable information to inform you of other products or services
-            available from Borderlands and its affiliates. Borderlands may also
-            contact you via surveys to conduct research about your opinion of current services
-            or of potential new services that may be offered.</p>
-            <p>Borderlands does not sell, rent or lease its customer lists to third parties.
-            Borderlands may, from time to time, contact you on behalf of external business
-            partners about a particular offering that may be of interest to you. In those cases,
-            your unique personally identifiable information (e-mail, name, address, telephone
-            number) is not transferred to the third party. In addition, Borderlands
-            may share data with trusted partners to help us perform statistical analysis, send
-            you email or postal mail, provide customer support, or arrange for deliveries. All
-            such third parties are prohibited from using your personal information except to
-            provide these services to Borderlands, and they are required to maintain
-            the confidentiality of your information.</p>
-            <p>Borderlands does not use or disclose sensitive personal information, such
-            as race, religion, or political affiliations, without your explicit consent.</p>
-            <p>Borderlands keeps track of the Web sites and pages our customers visit within
-            Borderlands, in order to determine what Borderlands services are
-            the most popular. This data is used to deliver customized content and advertising
-            within Borderlands to customers whose behavior indicates that they are interested
-            in a particular subject area.</p>
-            <p>Borderlands Web sites will disclose your personal information, without notice,
-            only if required to do so by law or in the good faith belief that such action is
-            necessary to: (a) conform to the edicts of the law or comply with legal process
-            served on Borderlands or the site; (b) protect and defend the rights or
-            property of Borderlands; and, (c) act under exigent circumstances to protect
-            the personal safety of users of Borderlands, or the public.</p>
-            <p><span>Use of Cookies</span></p>
-            <p>The Borderlands Web site use "cookies" to help you personalize your online
-            experience. A cookie is a text file that is placed on your hard disk by a Web page
-            server. Cookies cannot be used to run programs or deliver viruses to your computer.
-            Cookies are uniquely assigned to you, and can only be read by a web server in the
-            domain that issued the cookie to you.</p>
-            <p>One of the primary purposes of cookies is to provide a convenience feature to save
-            you time. The purpose of a cookie is to tell the Web server that you have returned
-            to a specific page. For example, if you personalize Borderlands pages, or
-            register with Borderlands site or services, a cookie helps Borderlands
-            to recall your specific information on subsequent visits. This simplifies the process
-            of recording your personal information, such as billing addresses, shipping addresses,
-            and so on. When you return to the same Borderlands Web site, the information
-            you previously provided can be retrieved, so you can easily use the Borderlands
-            features that you customized.</p>
-            <p>You have the ability to accept or decline cookies. Most Web browsers automatically
-            accept cookies, but you can usually modify your browser setting to decline cookies
-            if you prefer. If you choose to decline cookies, you may not be able to fully experience
-            the interactive features of the Borderlands services or Web sites you visit.</p>
-            <p><span>Security of your Personal Information</span></p>
-            <p>Borderlands secures your personal information from unauthorized access,
-            use or disclosure. Borderlands secures the personally identifiable information
-            you provide on computer servers in a controlled, secure environment, protected from
-            unauthorized access, use or disclosure. When personal information (such as a credit
-            card number) is transmitted to other Web sites, it is protected through the use
-            of encryption, such as the Secure Socket Layer (SSL) protocol.</p>
-            <p><span>Changes to this Statement</span></p>
-            <p>Borderlands will occasionally update this Statement of Privacy to reflect
-    company and customer feedback. Borderlands encourages you to periodically
-            review this Statement to be informed of how Borderlands is protecting your
-            information.</p>
-            <p><span>Contact Information</span></p>
-            <p>Borderlands welcomes your comments regarding this Statement of Privacy.
-            If you believe that Borderlands has not adhered to this Statement, please
-            contact Borderlands at <a href="mailto:stuart.fraser@carlisle.gov.uk">stuart.fraser@carlisle.gov.uk</a>.
-            We will use commercially reasonable efforts to promptly determine and remedy the
-            problem.</p>
+            <div className="container">
+
+                <p>Borderlands is committed to protecting your privacy and developing technology
+                that gives you the most powerful and safe online experience. This Statement of Privacy
+                applies to the Borderlands site and governs data collection and usage.
+                By using the Borderlands site, you consent to the data practices described
+                in this statement.</p>
+                <p><span>Collection of your Personal Information</span></p>
+                <p>Borderlands collects personally identifiable information, such as your email
+                address, name, home or work address or telephone number. Borderlands also
+                collects anonymous demographic information, which is not unique to you, such as
+                your ZIP code, age, gender, preferences, interests and favorites.</p>
+                <p>There is also information about your computer hardware and software that is automatically
+                collected by Borderlands. This information can include: your IP address,
+                browser type, domain names, access times and referring website addresses. This
+                information is used by Borderlands for the operation of the service, to
+                maintain quality of the service, and to provide general statistics regarding use
+                of the Borderlands site.</p>
+                <p>Please keep in mind that if you directly disclose personally identifiable information
+                or personally sensitive data through Borderlands public message boards,
+                this information may be collected and used by others. Note: Borderlands
+                does not read any of your private online communications.</p>
+                <p>Borderlands encourages you to review the privacy statements of Web sites
+                you choose to link to from Borderlands so that you can understand how those
+                Web sites collect, use and share your information. Borderlands is not responsible
+                for the privacy statements or other content on Web sites outside of the Borderlands
+                and Borderlands family of Web sites.</p>
+                <p><span>Use of your Personal Information</span></p>
+                <p>Borderlands collects and uses your personal information to operate the Borderlands
+                Web site and deliver the services you have requested. Borderlands also uses
+                your personally identifiable information to inform you of other products or services
+                available from Borderlands and its affiliates. Borderlands may also
+                contact you via surveys to conduct research about your opinion of current services
+                or of potential new services that may be offered.</p>
+                <p>Borderlands does not sell, rent or lease its customer lists to third parties.
+                Borderlands may, from time to time, contact you on behalf of external business
+                partners about a particular offering that may be of interest to you. In those cases,
+                your unique personally identifiable information (e-mail, name, address, telephone
+                number) is not transferred to the third party. In addition, Borderlands
+                may share data with trusted partners to help us perform statistical analysis, send
+                you email or postal mail, provide customer support, or arrange for deliveries. All
+                such third parties are prohibited from using your personal information except to
+                provide these services to Borderlands, and they are required to maintain
+                the confidentiality of your information.</p>
+                <p>Borderlands does not use or disclose sensitive personal information, such
+                as race, religion, or political affiliations, without your explicit consent.</p>
+                <p>Borderlands keeps track of the Web sites and pages our customers visit within
+                Borderlands, in order to determine what Borderlands services are
+                the most popular. This data is used to deliver customized content and advertising
+                within Borderlands to customers whose behavior indicates that they are interested
+                in a particular subject area.</p>
+                <p>Borderlands Web sites will disclose your personal information, without notice,
+                only if required to do so by law or in the good faith belief that such action is
+                necessary to: (a) conform to the edicts of the law or comply with legal process
+                served on Borderlands or the site; (b) protect and defend the rights or
+                property of Borderlands; and, (c) act under exigent circumstances to protect
+                the personal safety of users of Borderlands, or the public.</p>
+                <p><span>Use of Cookies</span></p>
+                <p>The Borderlands Web site use "cookies" to help you personalize your online
+                experience. A cookie is a text file that is placed on your hard disk by a Web page
+                server. Cookies cannot be used to run programs or deliver viruses to your computer.
+                Cookies are uniquely assigned to you, and can only be read by a web server in the
+                domain that issued the cookie to you.</p>
+                <p>One of the primary purposes of cookies is to provide a convenience feature to save
+                you time. The purpose of a cookie is to tell the Web server that you have returned
+                to a specific page. For example, if you personalize Borderlands pages, or
+                register with Borderlands site or services, a cookie helps Borderlands
+                to recall your specific information on subsequent visits. This simplifies the process
+                of recording your personal information, such as billing addresses, shipping addresses,
+                and so on. When you return to the same Borderlands Web site, the information
+                you previously provided can be retrieved, so you can easily use the Borderlands
+                features that you customized.</p>
+                <p>You have the ability to accept or decline cookies. Most Web browsers automatically
+                accept cookies, but you can usually modify your browser setting to decline cookies
+                if you prefer. If you choose to decline cookies, you may not be able to fully experience
+                the interactive features of the Borderlands services or Web sites you visit.</p>
+                <p><span>Security of your Personal Information</span></p>
+                <p>Borderlands secures your personal information from unauthorized access,
+                use or disclosure. Borderlands secures the personally identifiable information
+                you provide on computer servers in a controlled, secure environment, protected from
+                unauthorized access, use or disclosure. When personal information (such as a credit
+                card number) is transmitted to other Web sites, it is protected through the use
+                of encryption, such as the Secure Socket Layer (SSL) protocol.</p>
+                <p><span>Changes to this Statement</span></p>
+                <p>Borderlands will occasionally update this Statement of Privacy to reflect
+                company and customer feedback. Borderlands encourages you to periodically
+                review this Statement to be informed of how Borderlands is protecting your
+                information.</p>
+                <p><span>Contact Information</span></p>
+                <p>Borderlands welcomes your comments regarding this Statement of Privacy.
+                If you believe that Borderlands has not adhered to this Statement, please
+                contact Borderlands at <a href="mailto:stuart.fraser@carlisle.gov.uk">stuart.fraser@carlisle.gov.uk</a>.
+                We will use commercially reasonable efforts to promptly determine and remedy the
+                problem.</p>
+            </div>
             <Footer/>
         </>
     );
