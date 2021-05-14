4c50cf715c4733a938e98d4fb9c4010ac21d162a
Ken Joo <kenjoo.yeap@gmail.com>
2021-05-14 16:04:46 +0100

    styling: footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,9 +3,13 @@ import { Link } from "gatsby"
 
 const Footer = () => {
     return (
-        <div className="footer-container">
-            <Link to="/Terms" >Terms of Use </Link> : 
-            <Link to="/Privacy" > Privacy Status</Link>
+        <div className="footer-wrapper">
+            {/* <div className="footer-bottom"> */}
+                <div className="footer-container">
+                    <Link to="/Terms" className="anchor_style">Terms of Use </Link> : 
+                    <Link to="/Privacy" className="anchor_style"> Privacy Status</Link>
+                </div>
+            {/* </div> */}
         </div>
     )
 }
