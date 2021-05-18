d5ce1ea3c75e151447a7bf10539002b8d93d5dbd
Ken Joo <kenjoo.yeap@gmail.com>
2021-05-18 10:56:32 +0100

    styling: News accordion

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -102,59 +102,65 @@ export const AccordionNews = (props) => {
             m="20px 0"
             border="solid 3px #836098"
         >
-            <AccordionButton
-                display="flex"
-                border-bottom="solid 3px #836098"
-                color="#836098"   
-                bg="white"
-                fontSize= "28px"
-                padding="0"
-                lineHeight= {{base:"28px",lg:"22px"}}
-            >
-                <Box
-                    padding= "14px 25px"
-                    borderRight= "solid 3px #836098"
-                    cursor= "pointer"
-                    flexBasis= {{base:"85%",lg:"95%"}}
-                    textAlign="left"
-                >
-                    {title}
-                </Box>
-                <Box
-                    flexBasis= {{base:"15%",lg:"5%"}}
-                > 
-                    +
-                </Box>
-            </AccordionButton>
-            <AccordionPanel
-                padding="0"
-            >
-                <Box
-                    m="25px"
-                >
-                    {textArr.map((paragraph) => {
-                        return (
-                            <>
-                                <p>{paragraph}</p>
-                            </>
-                        )
-                    })}
-                </Box>
-                <Button
-                    border-top= "solid 3px #836098"
-                    text-align= "center"
-                    padding= "30px"
-                    color= "#836098"
-                    font-size= "24px"
-                    bg="white"
-                    w="100%"
-                    borderTop="solid 3px #836098"
-                    borderRadius="0"
-                    fontSize= "24px"
-                >
-                    Read More
-                </Button>
-            </AccordionPanel>
+            {({ isExpanded }) => (
+                <>
+                    <AccordionButton
+                        // className="container-table-bottom"
+                        display="flex"
+                        borderBottom= {isExpanded?"solid 3px #836098":"0"}
+                        color="#836098"   
+                        bg="white"
+                        fontSize= "28px"
+                        padding="0"
+                        lineHeight= {{base:"28px",lg:"22px"}}
+                        _expanded = {{bg: "white", color:"black"}}
+                        _last = "none"
+                    >
+                        <Box
+                            padding= "14px 25px"
+                            borderRight= "solid 3px #836098"
+                            cursor= "pointer"
+                            flexBasis= {{base:"85%",lg:"95%"}}
+                            textAlign="left"
+                        >
+                            {title}
+                        </Box>
+                        <Box
+                            flexBasis= {{base:"15%",lg:"5%"}}
+                        > 
+                            {isExpanded? "-":"+"}
+                        </Box>
+                    </AccordionButton>
+                    <AccordionPanel
+                        padding="0"
+                    >
+                        <Box
+                            m="25px"
+                        >
+                            {textArr.map((paragraph) => {
+                                return (
+                                    <>
+                                        <p>{paragraph}</p>
+                                    </>
+                                )
+                            })}
+                        </Box>
+                        <Button
+                            border-top= "solid 3px #836098"
+                            padding= "30px"
+                            color= "#836098"
+                            fontSize= "24px"
+                            bg="white"
+                            w="100%"
+                            borderTop="solid 3px #836098"
+                            borderRadius="0"
+                            fontSize= "24px"
+                        >
+                            Read More
+                        </Button>
+                    </AccordionPanel>
+                </>
+            )}
         </AccordionItem>
     )
 }
