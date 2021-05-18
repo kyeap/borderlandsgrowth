96b8e792736c140fb4d1ebd1bf6e9c65d5f42b05
Ken Joo <kenjoo.yeap@gmail.com>
2021-05-18 09:57:09 +0100

    implement react-spring

--- src/components/DropdownCard.js
+++ src/components/DropdownCard.js
@@ -1,10 +1,17 @@
 import React, {useState} from "react";
 import "../styles/dropdownCard.css";
+import { useSpring, animated } from "react-spring";
 
 
 const DropdownCard = (props) => {
     const {date, title, text = "", download = "", link } = props;
     const [dropdown, setDropdown] = useState(false);
+
+    const motion = useSpring({ 
+        to: { opacity: dropdown? 1:0, height: dropdown? 100 : 0 }, 
+        from: { opacity: dropdown? 0:1, height: dropdown? 0 : 100 },
+        reset: true,
+    });
 
     return (
         <div className="dropdowncard-container">
@@ -14,19 +21,20 @@ const DropdownCard = (props) => {
                  + 
                 </div>
             </div>
-            <div id={title} className="dropdown-container" style={{ height: dropdown? "auto" : "0px", display: dropdown? "block":"none"}}>
-                <div>{date}</div>
-                <div>{title}</div>
-                {text != "" && <div>{text}</div>}
-                {download != "" && 
-                    <div className="download-container">
-                        <div>Documents to download</div>
-                        <div>{download}</div>
-                    </div>
-                }
-                <button><a href={link}>READ MORE</a></button>    
-            </div>
-                 
+            <animated.div style={motion} id={title} >
+                {/* <div id={title} className="dropdown-container" style={{ height: dropdown? "auto" : "0px", display: dropdown? "block":"none"}}> */}
+                    <div>{date}</div>
+                    <div>{title}</div>
+                    {text != "" && <div>{text}</div>}
+                    {download != "" && 
+                        <div className="download-container">
+                            <div>Documents to download</div>
+                            <div>{download}</div>
+                        </div>
+                    }
+                    <button><a href={link}>READ MORE</a></button>    
+                {/* </div> */}
+            </animated.div>
         </div>
     )
 }
